dd07ea3146967e5eb7281a31051eaaf1e8d78dca
manueldembo <madembo18@gmail.com>
2025-02-27 17:04:45 +0100

    fix: remove jwtmodule

--- src/infra/controllers/auth.module.ts
+++ src/infra/controllers/auth.module.ts
@@ -3,16 +3,9 @@ import { BcryptAdapter } from '../bcrypt-adapter';
 import { UserRepositoryPostgres } from '../precistency/postgres/user.repository';
 import { AuthController } from './auth.controller';
 import { LoginUsecase } from 'src/application/usecase/login.usecase';
-import { JwtModule } from '@nestjs/jwt';
 import { JWTTokenService } from '../services/jwt.service';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '24h' },
-    }),
-  ],
   controllers: [AuthController],
   providers: [
     LoginUsecase,
